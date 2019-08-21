f3cb034d628642dd69223d808ad252a2b1223428
Renato Filho <renatosousafilho@gmail.com>
2019-08-21 13:32:41 -0300

    crud users

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'renato',
+    password: 'docker',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+
+}
