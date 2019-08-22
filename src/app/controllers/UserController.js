289a8c646789448bdd103ff5931431edde923786
Renato Filho <renatosousafilho@gmail.com>
2019-08-21 21:00:47 -0300

    validar campos do post com Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
+
 import User from '../models/User'
+
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+        })
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({error: 'Validation fails'})
+        }
+
         const userExists = await User.findOne({where: { email: req.body.email} });
         
         if (userExists) {
@@ -18,6 +31,22 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string().when('oldPassword', (oldPassword, field) => 
+                oldPassword ? field.required() : field
+            ),
+            confirmPassword: Yup.string().min(6).when('password', (password, field) => 
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            )
+        })
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({error: 'Validation fails'})
+        }
+
         const user = await User.findByPk(req.userId)
 
         const { email, oldPassword } = req.body;
