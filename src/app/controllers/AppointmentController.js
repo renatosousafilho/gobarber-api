a0732828382734336e89068479f2d90ecb367811
Renato Filho <renatosousafilho@gmail.com>
2019-08-26 21:55:01 -0300

    Enviar e-mail quando cancelar agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
     async index(req, res) {
         const { page = 1 } = req.query;
@@ -103,7 +105,15 @@ class AppointmentController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email'],
+                }                
+            ]
+        });
 
         // check if user signed is owner from appointment
         if (req.userId !== appointment.user_id) {
@@ -120,6 +130,12 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento cancelado',
+            text: 'Você tem um novo cancelamento',
+        });
 
         return res.json(appointment);
     }
